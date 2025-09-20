b70fc4612415509dc56c80d37121ec52ecd4874d
maearl123 <maearl123@gmail.com>
2025-09-20 08:10:01 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import AdminLayout from "../../components/AdminLayout";
+import AdminLayout from "../components/AdminLayout";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
